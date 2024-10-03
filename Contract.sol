757382dbc70ec3e1930d7dc55ed280731c265ed6
Jzzz <90880791+eljezdanna@users.noreply.github.com>
2024-10-03 11:15:16 +0700

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -1,1 +1,13 @@
-// contract here
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MyToken is ERC20, ERC20Burnable, ERC20Permit {
+    constructor() ERC20("My Token", "MTK") ERC20Permit("My Token") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+}
